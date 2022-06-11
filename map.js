830cb147c111be2c10b2f3e3d2afc4aae7f1261c
marktodman <drmtodman@gmail.com>
2022-06-11 15:20:41 +0000

    Final challenge

--- map.js
+++ map.js
@@ -5,16 +5,27 @@
  */
 
 // Using a for loop
-
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+console.log(results);
 
 // Using map()
-
+const multByTwo = function (num) {
+  return num * 2;
+}
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
-
+const mapResults2 = nums.map(function (num) { return num * 2 });
+console.log(mapResults2);
 
 // Simplfied w/ map() + arrow function
-
+const mapResults3 = nums.map(num => num * 2);
+console.log(mapResults3);
 
 // With objects:
 const students = [
@@ -37,3 +48,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(student => [student.name, student.id]);
+console.log(studentsWithIds)
